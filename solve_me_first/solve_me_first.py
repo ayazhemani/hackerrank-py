099ead4ab002e781b543d83a13950e97214c28b4
Ayaz Hemani <ayazhemani@gmail.com>
2018-05-01 23:59:08 -0400

    Challenge Passed

---
+++ solve_me_first/solve_me_first.py
@@ -0,0 +1,24 @@
+"""Compute the sum of two integers.
+"""
+
+def solveMeFirst(x_1, y_1):
+    """Function to compute sum of two numbers.
+
+    Args:
+        x_1 (int): The first integer.
+        y_1 (int): The second integer.
+
+    Returns:
+        int: Sum of x_1 and y_1.
+    """
+    return x_1 + y_1
+
+def main():
+    """Get inputs from stdin and print to stdout
+    """
+    x_1 = input()
+    y_1 = input()
+    print solveMeFirst(x_1, y_1)
+
+if __name__ == '__main__':
+    main()
