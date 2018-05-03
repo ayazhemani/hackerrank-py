757aa0a5e0c4a6f12d970f13faaa053b12265ecf
Ayaz Hemani <ayazhemani@gmail.com>
2018-05-02 23:11:04 -0400

    Fexed Diagonal Difference
    Added Comments and Test Cases

--- diagonalDifference/diagonalDifference.py
+++ diagonalDifference/diagonalDifference.py
@@ -6,11 +6,28 @@ def diagonal_difference(matrix_len, matrix_array):
     between the sum of the diagonals.
 
     Args:
-        matrix_len (TYPE): size of the matrix square - length and height.
-        matrix_array (TYPE): 2 dimensional integer array for matrix
+        matrix_len (int): size of the matrix square - length and height.
+        matrix_array (int[][]): 2 dimensional integer array for matrix
 
     Returns:
         int: absolute value of difference in diagonals
+
+    Example:
+        Given a matrix_array of matrix_len = n, (n = i = j):
+
+            a_0_0   a_0_1   a_0_2   ...     a_0_j
+            a_1_0   a_1_1   a_1_2   ...     a_1_j
+            a_2_0   a_2_1   a_2_2   ...     a_2_j
+            ...     ...     ...     ...     ...
+            a_i_0   a_i_1   a_i_2   ...     a_i_j
+
+        The absolute difference equals the primary diagonal subtracted by the secondary diagonal:
+            abs_difference = abs(diagonal_1 - diagonal_2)
+
+        Where:
+            diagonal_1 can be computed by adding all instances in the array where i == j
+
+            diagonal_2 can be computed by adding all instances in the array where i + j == n - 1
     """
 
     difference = 0
--- diagonalDifference/test_diagonalDifference.py
+++ diagonalDifference/test_diagonalDifference.py
@@ -9,12 +9,46 @@ class TestDiagonalDifference(unittest.TestCase):
     """
     def test_hackerrank_sample(self):
         """Validate absolute difference of given matrix.
-            [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
-            Difference = 15
+        [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
+        Absolute Difference = 15
         """
         result = diagonal_difference(3, [[11, 2, 4], [4, 5, 6],
                                          [10, 8, -12]])
         self.assertEquals(result, 15)
+
+    def test_no_matrix(self):
+        """Validate matrix of length zero
+        [[]]
+        Absolute Difference = 0
+        """
+        result = diagonal_difference(0, [[]])
+        self.assertEquals(result, 0)
+
+    def test_neg_matrix(self):
+        """Validate matrix of negative integers
+        [[-11, -2, -4], [-4, -5, -6], [-10, -8, -12]]
+        Absolute Difference = 9
+        """
+        result = diagonal_difference(3, [[-11, -2, -4],
+                                         [-4, -5, -6],
+                                         [-10, -8, -12]])
+        self.assertEquals(result, 9)
+
+    def test_5x5_matrix(self):
+        """Validate matrix of length 5
+        [[1, 2, 3, 4, 5],
+         [6, 7, 8, 9, 10],
+         [11, 12, 13, 14, 15],
+         [16, 17, 18, 19, 20],
+         [21, 22, 23, 24, 25]]
+         Absolute Difference = 0
+        """
+        result = diagonal_difference(5, [[1, 2, 3, 4, 5],
+                                         [6, 7, 8, 9, 10],
+                                         [11, 12, 13, 14, 15],
+                                         [16, 17, 18, 19, 20],
+                                         [21, 22, 23, 24, 25]])
+        self.assertEquals(result, 0)
 
 if __name__ == '__main__':
     unittest.main(exit=False)
