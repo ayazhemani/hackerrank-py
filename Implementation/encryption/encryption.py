21105c9e87b79c5cc8bfccbce26fe02a4b49834d
Ayaz Hemani <ayazhemani@gmail.com>
2018-05-26 23:11:38 -0400

    Solved Encryption

--- Implementation/encryption/encryption.py
+++ Implementation/encryption/encryption.py
@@ -1,15 +1,38 @@
 """Solution for HackerRank challenge: Encryption
 """
+import math
 
-def encrypt():
-    """Description
+def encrypt(str):
+    """Given a string rewrite the string as a block of text and return the columns of the block.
+
+    Where:
+        The block width is the ceiling of the sqrt of the string length.
+        The block length is the floor of the sqrt of the string length.
+
+    Args:
+        str (string): Sentence to encrypt with spaces removed.
+
+    Returns:
+        string[]: Columns of block of text.
     """
-    return 0
+    y_len = int(math.floor(len(str) ** 0.5))
+    x_len = int(math.ceil(len(str) ** 0.5))
+    while len(str) > y_len*x_len:
+        y_len += 1
+    str = str.ljust(y_len*x_len)
+
+    result = []
+    for i in range(x_len):
+        result.append(str[i::x_len].replace(' ', ''))
+
+    return result
 
 def main():
     """Receives input from stdin, provides output to stdout.
     """
-    print encrypt()
+    input_string = raw_input().replace(' ', '')
+    for word in encrypt(input_string):
+        print word,
 
 if __name__ == '__main__':
     main()
--- Implementation/encryption/test_encryption.py
+++ Implementation/encryption/test_encryption.py
@@ -5,13 +5,31 @@ import unittest
 from encryption import encrypt
 
 class TestEncryption(unittest.TestCase):
-    """Description
+    """Validate columns of blocks for given string.
     """
-    def test_sample(self):
-        """Description
+    def test_hackerrank_sample1(self):
+        """Validate provided test.
         """
-        result = encrypt()
-        self.assertEquals(result, None)
+        result = encrypt('haveaniceday')
+        self.assertEquals(result, ['hae', 'and', 'via', 'ecy'])
+
+    def test_hackerrank_sample2(self):
+        """Validate provided test.
+        """
+        result = encrypt('feedthedog')
+        self.assertEquals(result, ['fto', 'ehg', 'ee', 'dd'])
+
+    def test_hackerrank_sample3(self):
+        """Validate provided test.
+        """
+        result = encrypt('chillout')
+        self.assertEquals(result, ['clu', 'hlt', 'io'])
+
+    def test_hackerrank_sample4(self):
+        """Validate provided test.
+        """
+        result = encrypt('iffactsdontfittotheorychangethefacts')
+        self.assertEquals(result, ['isieae', 'fdtonf', 'fotrga', 'anoyec', 'cttctt', 'tfhhhs'])
 
 if __name__ == '__main__':
     unittest.main(exit=False)
