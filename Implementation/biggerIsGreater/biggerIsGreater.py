a3367a6ecb3dba751f1445128805fefdb169653d
Ayaz Hemani <ayazhemani@gmail.com>
2018-05-30 23:48:47 -0400

    Solved Bigger Is Greater

--- Implementation/biggerIsGreater/biggerIsGreater.py
+++ Implementation/biggerIsGreater/biggerIsGreater.py
@@ -12,47 +12,11 @@ def bigger_is_greater(str):
     Returns:
         TYPE: Description
     """
-
-
-    base_val = get_lexicographical_value(str)
-    results = {}
-    for permutation in permutations(str):
-        val = get_lexicographical_value(permutation)
-        if val > base_val:
-            results[val] = permutation
-
-    keylist = results.keys()
-    keylist.sort()
-
-    if not keylist:
+    initial_sequence = [ord(ltr) for ltr in str]
+    final_sequence = next_permutation(initial_sequence)
+    if not final_sequence:
         return 'no answer'
-
-    return ''.join(results[keylist[0]])
-
-    # START DANS CODE
-    # str_len = len(str)
-
-    # for i in xrange(1, str_len):
-    #     if i == str_len - 1:
-    #         temp = ord(str[0])
-    #         templist = [ord(letter) for letter in str]
-    #         new_templist = [ord(letter) for letter in str if ord(letter) > temp]
-    #         templist.sort()
-    #         new_templist.sort()
-    #         if len(new_templist) == 0:
-    #             return 'no answer'
-    #         templist.remove(new_templist[0])
-    #         return chr(new_templist[0]) + ''.join([chr(ltr) for ltr in templist])
-
-    #     elif ord(str[str_len - i]) > ord(str[str_len - i - 1]):
-    #         # print str[str_len - i], str[str_len - i - 1], ord(str[str_len - i]), ord(str[str_len - i - 1])
-    #         tempy = [letter for letter in str]
-    #         tempy[str_len - i] = str[str_len - i - 1]
-    #         tempy[str_len - i - 1] = str[str_len - i]
-    #         return ''.join(tempy)
-    # # END DANS CODE
-
-    return str
+    return ''.join(map(chr, final_sequence))
 
 def next_permutation(arr):
     # Find non-increasing suffix
@@ -71,20 +35,6 @@ def next_permutation(arr):
     # Reverse suffix
     arr[i : ] = arr[len(arr) - 1 : i - 1 : -1]
     return arr
-
-def get_lexicographical_value(lex_str):
-    """Summary
-
-    Args:
-        lex_str (TYPE): Description
-
-    Returns:
-        TYPE: Description
-    """
-    val = 0
-    for i in xrange(len(lex_str)):
-        val += (ord(lex_str[i]) - ord('`')) * (10 ** (len(lex_str) - i - 1))
-    return val
 
 def main():
     """Receives input from stdin, provides output to stdout.
